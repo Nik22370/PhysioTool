e115a74f0e82715e36a58769555a98dd125efff3
Michael Schletz <schletz@spengergasse.at>
2022-11-29 14:11:01 +0100

    Created calendar view

--- Physiotool/Physiotool.Client/src/main.js
+++ Physiotool/Physiotool.Client/src/main.js
@@ -1,11 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from "axios";
+import process from 'node:process';
 
+axios.defaults.baseURL = process.env.NODE_ENV == 'production' ? "/api" : "http://localhost:5000/api";
 import './assets/main.css'
-
 const app = createApp(App)
-
 app.use(router)
-
 app.mount('#app')
---
+++ Physiotool/Physiotool.Client/src/views/CalendarView.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="calendar">
+        <h1>Kalender</h1>
+        <div v-for="day in days" v-bind:key="day.timestamp">
+            {{ day.germanDate }}
+        </div>
+    </div>
+</template>
+
+<style scoped>
+</style>
+
+<script>
+import axios from 'axios';
+
+export default {
+    data() {
+        return {
+            days: [],
+        };
+    },
+    async mounted() {
+        const res = await axios.get('calendar/2022/1');
+        this.days = res.data;
+    },
+};
+</script>
